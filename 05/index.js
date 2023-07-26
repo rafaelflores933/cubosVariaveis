b3b6094988840e4dbb4c15518823fbd090614af6
Rafael <rafaelflores933@gmail.com>
2023-07-25 22:13:06 -0300

    finalizado exercicio 5

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,9 @@
 
+function distanciaEntrePontos(x1, x2, y1, y2) {
+    x = x2 - x1
+    y = y2 - y1
+    xy = (x ** 2) + (y ** 2)
+    distancia = Math.sqrt(xy)
+    return distancia
+}
+console.log(distanciaEntrePontos(1, 1, 1, 4))
